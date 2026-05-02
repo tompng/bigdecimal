90078d602cc940c76e566c0b2848cc4dd2a784cd
tompng <tomoyapenguin@gmail.com>
2026-05-03 04:18:50 +0900

    Add more comments + small fixes

--- lib/bigdecimal/math/gamma.rb
+++ lib/bigdecimal/math/gamma.rb
@@ -18,7 +18,7 @@ module BigMath
   #   O(PREC*log(PREC)^3*log(factorial_argument))
   #
   # Stirling's approximation with Bernoulli numbers
-  #   Only used when x is large enough.
+  #   Only used when x is extremely large.
 
   module Gamma # :nodoc:
 
@@ -40,7 +40,7 @@ module BigMath
       else
         base, large_factorial_arg, small_factorial_arg = gamma_lagrange(x, prec2)
         base.mult(
-          prod_range(1..small_factorial_arg, prec2),
+          bsm_prod(1..small_factorial_arg, prec2),
           prec2
         ).mult(
           integer_factorial(large_factorial_arg, prec2),
@@ -86,6 +86,9 @@ module BigMath
 
         prec2 += [-diff1_exponent, -diff2_exponent, 0].max
 
+        # When x is extremely large, the cost of Bernoulli number generation for Stirling's
+        # asymptotic expansion is smaller than the cost of multiple steps of doubling method.
+        # The condition is based on heuristic cost estimation and empirical tuning.
         if x > prec2 && x.exponent > Integer.sqrt(prec2) / 6
           [lgamma_stirling(x, prec2).mult(1, prec), 1]
         elsif x.frac.zero?
@@ -93,7 +96,7 @@ module BigMath
         else
           base, large_factorial_arg, small_factorial_arg = gamma_lagrange(x, prec2)
           lgamma = BigMath.log(base, prec2).add(
-            BigMath.log(prod_range(1..small_factorial_arg, prec2), prec2),
+            BigMath.log(bsm_prod(1..small_factorial_arg, prec2), prec2),
             prec2
           ).add(
             integer_factorial_log(large_factorial_arg, prec2),
@@ -104,7 +107,7 @@ module BigMath
       end
     end
 
-    # Calculates prod{x-k} and its coefficients for given ks, xn and prec with baby-step giant-step method.
+    # Calculates prod { x - k } and its coefficients for given ks, xn and prec with baby-step giant-step method.
     # xn is an array of precalculated powers of x: [1, x, x**2, x**3, ...]
     def self.x_minus_k_prod_coef(ks, xn, prec) # :nodoc:
       coef = [1]
@@ -125,24 +128,24 @@ module BigMath
       [prod, coef]
     end
 
-    # Calculate range.reduce(:*) in a given precision by binary splitting method.
-    def self.prod_range(range, prec)
-      numbers = range.map {|i| BigDecimal(i) }
+    # Calculate numbers.reduce(:*) in a given precision by Binary Splitting Method.
+    def self.bsm_prod(numbers, prec)
+      numbers = numbers.map {|i| BigDecimal(i) }
       numbers = numbers.each_slice(2).map {|a, b| b ? a.mult(b, prec) : a } while numbers.size > 1
       return numbers.first || BigDecimal(1)
     end
 
     # Calculate factorial for integer n
     def self.integer_factorial(n, prec)
-      power_part, exp2, expsqrtpi = integer_factorial_parameter(n, prec)
+      power_part, exp2, exp_sqrtpi = integer_factorial_parameter(n, prec)
       ans = BigDecimal(2).power(exp2, prec)
       power_part.each_with_index do |base, index|
         ans = ans.mult(base.power(1 << index, prec), prec)
       end
-      if expsqrtpi != 0
+      if exp_sqrtpi != 0
         pi = BigMath::PI(prec)
-        pipow = pi.power(expsqrtpi / 2, prec)
-        pipow = pipow.mult(pi.sqrt(prec), prec) if expsqrtpi.odd?
+        pipow = pi.power(exp_sqrtpi / 2, prec)
+        pipow = pipow.mult(pi.sqrt(prec), prec) if exp_sqrtpi.odd?
         ans = ans.div(pipow, prec)
       end
       ans
@@ -150,60 +153,71 @@ module BigMath
 
     # Calculate log factorial for integer n
     def self.integer_factorial_log(n, prec)
-      power_part, exp2, expsqrtpi = integer_factorial_parameter(n, prec)
+      power_part, exp2, exp_sqrtpi = integer_factorial_parameter(n, prec)
       ans = BigMath.log(2, prec) * exp2
       power_part.each_with_index do |base, index|
         ans = ans.add(BigMath.log(base, prec) * (1 << index), prec)
       end
-      if expsqrtpi != 0
+      if exp_sqrtpi != 0
         pi = BigMath::PI(prec)
-        ans = ans.sub(BigMath.log(pi, prec) * (BigDecimal(expsqrtpi) / 2), prec)
+        ans = ans.sub(BigMath.log(pi, prec) * (BigDecimal(exp_sqrtpi) / 2), prec)
       end
       ans
     end
 
     # Calculates parameters for integer factorial calculation.
-    # Returns [base_power_part, exp2, expsqrtpi] that can produce factorial(n) as:
-    #   factorial(n) = prod { base_power_part[i]**(1 << i) } * 2**exp2 / sqrt(pi)**expsqrtpi
+    # Returns [base_power_part, exp2, exp_sqrtpi] that can produce factorial(n) as:
+    #   factorial(n) = prod { base_power_part[i]**(1 << i) } * 2**exp2 / sqrt(pi)**exp_sqrtpi
+    # These parameters are used to avoid overflow when calculating log factorial and lgamma for large n.
     def self.integer_factorial_parameter(n, prec)
-      base_power_part, factorial_power_part, exp2, expsqrtpi = integer_factorial_recursive(n, prec)
+      base_power_part, factorial_power_part, exp2, exp_sqrtpi = integer_factorial_recursive(n, prec)
       fact_x = 1
       fact_y = BigDecimal(1)
       factorial_power_part.each_with_index do |factorial_arg, index|
-        fact_y = fact_y.mult(prod_range(fact_x + 1..factorial_arg, prec), prec)
+        fact_y = fact_y.mult(bsm_prod(fact_x + 1..factorial_arg, prec), prec)
         fact_x = factorial_arg
         base_power_part[index] = base_power_part[index].mult(fact_y, prec)
       end
-      [base_power_part, exp2, expsqrtpi]
+      [base_power_part, exp2, exp_sqrtpi]
     end
 
-    # Returns [base_power_part, factorial_power_part, exp2, expsqrtpi] that can produce factorial(n) as:
-    # factorial(n) = prod { base_power_part[i]**(1 << i) } * prod { factorial(factorial_power_part[i])**(1 << i) } * 2**exp2 / sqrt(pi)**(expsqrtpi)
+    # Returns [base_power_part, factorial_power_part, exp2, exp_sqrtpi] that can produce factorial(n) as:
+    # factorial(n) = prod { base_power_part[i]**(1 << i) } * prod { factorial(factorial_power_part[i])**(1 << i) } * 2**exp2 / sqrt(pi)**(exp_sqrtpi)
     # If n is large, this method recursively calculates factorial for smaller n by Legendre duplication formula
     def self.integer_factorial_recursive(n, prec)
       if n < 4 * prec
-        return [[prod_range(1..n, prec)], [], 0, 0]
+        return [[bsm_prod(1..n, prec)], [], 0, 0]
       end
 
       # Use Legendre duplication formula to calculate double factorials:
       #   factorial(n) = factorial(n/2)*factorial((n-1)/2)*2**n/sqrt(pi)
       base, large_factorial_arg, small_factorial_arg = gamma_lagrange_n_plus_half((n + 1) / 2, prec)
 
-      base = base.mult(prod_range(large_factorial_arg + 1..n / 2, prec), prec)
-      base_power_part, factorial_power_part, exp2, expsqrtpi = integer_factorial_recursive(large_factorial_arg, prec)
+      base = base.mult(bsm_prod(large_factorial_arg + 1..n / 2, prec), prec)
+      base_power_part, factorial_power_part, exp2, exp_sqrtpi = integer_factorial_recursive(large_factorial_arg, prec)
       [
         [base] + base_power_part,
         [small_factorial_arg] + factorial_power_part,
         exp2 * 2 + n,
-        expsqrtpi * 2 + 1
+        exp_sqrtpi * 2 + 1
       ]
     end
 
-    # Estimate required l for Lagrange interpolation to achieve the given precision.
+    # Estimate l that controls the required number of interpolation points `l` to achieve `prec` digits.
+    #
+    # The function b^x/x! approximates a Gaussian e^(-y^2 / 2b) around its peak (x=b).
+    # The Taylor coefficient of degree 2l is roughly c_2l = 1 / (l! * (2b)^l).
+    # Multiplying this by the distance product of 2l+1 nodes (approx (l/e)^(2l)), 
+    # the overall truncation error E is bounded by: E ~ (l / 2eb)^l.
+    #
+    # Setting E <= 10^-prec gives the implicit equation:
+    #   l * log10(2 * e * b / l) = prec  =>  l = prec / log10(2 * e * b / l)
     def self.gamma_lagrange_l(b, prec)
+      # Initial guess of l. When b >= 2 * prec, this initial guess is larger than the actual l.
       l = prec
+      # Solves the implicit equation by iteration.
       2.times { l = prec / Math.log10(2 * Math::E * b / l) }
-      l.ceil + 10
+      l.ceil + 10 # Adds safety margin
     end
 
     # Calculate approximate gamma by Lagrange interpolation of f(x) = b**x / x!
@@ -221,8 +235,11 @@ module BigMath
     # Returns [base, large_factorial_arg, small_factorial_arg] that can produce gamma(x) as:
     #   gamma(x) = base * factorial(large_factorial_arg) * factorial(small_factorial_arg)
     def self.gamma_lagrange(x, prec) # :nodoc:
-      # Shift x to ensure the scaled bell curve b**x/x! is wide enough to cover
-      # all 2*l+1 interpolation nodes without losing significant digits.
+      # Shift x to ensure all 2*l + 1 interpolation nodes strictly stay in the positive domain.
+      # While x > 1.3 * prec is the strict minimum to avoid poles at x <= 0 (since l <= prec),
+      # we heuristically use 2 * prec. A slightly larger base flattens the b^x/x! curve,
+      # will reduce `l` and also reduce total computation cost.
+      # Required `l` for `x = 2 * prec` is about `prec`.
       shift = x < 2 * prec ? 2 * prec - x.floor : 0
       x += shift
 
@@ -233,9 +250,9 @@ module BigMath
       # --- Reference: Naive interpolation logic ---
       # Optimize this calculation for full-digit-x case and small-digit-x case.
       # sum = BigDecimal(0)
-      # prod = [*(b-l)..(b+l), *(0...shift)].map {|i| x - i }.reduce { _1.mult(_2, prec) }
+      # prod = [*(b - l..b + l), *(0...shift)].map {|i| x - i }.reduce { _1.mult(_2, prec) }
       # c = BigDecimal(1) # represents w_i * f(x_i) (normalized)
-      # ((b-l)..(b+l)).each do |i|
+      # (b - l..b + l).each do |i|
       #   if i != b - l
       #     c = c.mult(-b * (b + l - i + 1), prec).div((i - b + l) * i, prec)
       #   end
@@ -243,11 +260,18 @@ module BigMath
       # end
       # --------------------------------------------
 
-      if x.n_significant_digits > prec / prec.bit_length
-        # Reduce full-precision multiplications/divisions by Baby-Step Giant-Step method
+      # When x has many significant digits, there will be `l * x.n_significant_digits / prec` full-digit multiplications in BSM.
+      # If the total cost exceeds `prec * l`, which is the cost of BSGS, we should use BSGS.
+      if x.n_significant_digits * prec.bit_length > prec
+        # Reduce full-precision multiplications/divisions using a Batched Evaluation
+        # inspired by the Baby-Step Giant-Step (BSGS) method.
 
+        # Normal BSGS uses batch_size = sqrt(l), but in this case, the coefficients of
+        # expanded prod { x - k } for each batch will be huge if batch_size is large.
+        # batch_size = log2(prec) is enough to make the BSGS cost to be O(prec * l).
         batch_size = prec.bit_length
-        # When expanding prod{x-k}, the coefficient of x**n might be huge.
+
+        # When expanding prod { x - k }, the coefficient of x**n might be huge.
         # Increase internal calculation precision to avoid catastrophic cancellation.
         internal_xn_prec = prec + (Math.log10(b + l) * batch_size).ceil
         xn = [BigDecimal(1)]
@@ -257,11 +281,11 @@ module BigMath
         sum = BigDecimal(0)
         prod = BigDecimal(1)
 
-        ((b-l)..(b+l)).to_a.each_slice(batch_size) do |batch_ks|
-          # Calculate prod{x-k} in this batch
+        ((b - l)..(b + l)).to_a.each_slice(batch_size) do |batch_ks|
+          # Calculate prod{ x - k } in this batch
           batch_prod, prod_coef = x_minus_k_prod_coef(batch_ks, xn, internal_xn_prec)
 
-          # Calculate coefficients of batch_prod / (x-k) using Synthetic Division (Ruffini's rule)
+          # Calculate coefficients of batch_prod / (x - k) using Synthetic Division (Ruffini's rule)
           batch_coef = [0] * batch_ks.size
           c_scale = 1r
           batch_ks.each do |k|
@@ -290,9 +314,8 @@ module BigMath
         end
       else
         # Binary Splitting Method (BSM) for short-digit inputs
-        prods = ((b-l)..(b+l)).map {|i| x - i } + shift.times.map {|i| x - i }
-        prods = prods.each_slice(2).map {|a, b| b ? a.mult(b, prec) : a } while prods.size != 1
-        prod = prods.first
+
+        prod = bsm_prod((b - l..b + l).map {|i| x - i } + shift.times.map {|i| x - i }, prec)
 
         # State represents a partial evaluation of the series as: [sum_num, mult_num, den]
         # Conceptually, each state translates to the following mathematical expression:
@@ -327,17 +350,20 @@ module BigMath
       [base, large_factorial_arg, small_factorial_arg]
     end
 
-    # Specialized version of gamma_lagrange(n + 0.5, prec) for integer n. n > 2 * prec is required.
+    # Specialized version of gamma_lagrange(n + 0.5, prec) for integer n.
+    # n should be large enough that it doesn't need shift operation which is omitted in this method.
     # Computation complexity: O(PREC*log(PREC)^3)
     # Same algorithm and return format as gamma_lagrange, but several times faster.
     # Used for double factorial calculation in integer factorial for large n.
     def self.gamma_lagrange_n_plus_half(n, prec) # :nodoc:
       b = n
       l = gamma_lagrange_l(b, prec)
-      prods = ((b-l)..(b+l)).map {|i| 2 * n - 2 * i - 1 }
+      prods = (b - l..b + l).map {|i| 2 * n - 2 * i - 1 }
       prods = prods.each_slice(2).map {|a, b| b ? a * b : a } while prods.size != 1
       prod = BigDecimal(prods.first).mult(BigDecimal(0.5).power(2 * l + 1, prec), prec)
 
+      # Exactly the same as gamma_lagrange but with x = n + 0.5 and some simplification.
+      # numerator and denominator are doubled to make them integers.
       fractions = (b - l + 1..b + l).map do |i|
         denominator = (2 * n - 1 - 2 * i) * ((i - b + l) * i)
         numerator = (2 * n + 1 - 2 * i) * (-b * (b + l - i + 1))
@@ -349,7 +375,9 @@ module BigMath
           v0 = a[0] * b[2] + a[1] * b[0]
           v1 = a[1] * b[1]
           v2 = a[2] * b[2]
-          s = v2.bit_length - prec * 10 / 3 # 10 / 3 = log2(10) + safety margin
+          # Drop lower bits to avoid denominator growing too large. It only needs prec * log2(10) bits.
+          # 10 / 3 is an approximation of log2(10) with some safety margin
+          s = v2.bit_length - prec * 10 / 3
           if s > 0
             v0 >>= s
             v1 >>= s
@@ -359,7 +387,7 @@ module BigMath
         end
       end
       fraction = fractions.first
-      sum = BigDecimal((fraction[0] + fraction[1]) * 2).div(fraction[2] * (2 * (n  - b + l) - 1), prec)
+      sum = BigDecimal((fraction[0] + fraction[1]) * 2).div(fraction[2] * (2 * (n - b + l) - 1), prec)
       base = BigDecimal(b).power(n - b + l, prec).div(BigDecimal(b).sqrt(prec).mult(sum.mult(prod, prec), prec), prec)
       large_factorial_arg = b - l
       small_factorial_arg = 2 * l
@@ -383,8 +411,9 @@ module BigMath
       )
     end
 
-    # Stirling's approximation with Bernoulli numbers for lgamma when x is large.
-    # Precondition: x > prec * ln(10) / 2 / pi
+    # Calculate gamma using Stirling's asymptotic expansion.
+    # While the condition of this asymptotic expansion is x > prec * log(10) / 2 / pi,
+    # we'll use this method only when x is extremely large to reduce the cost of Bernoulli number generation.
     def self.lgamma_stirling(x, prec) # :nodoc:
       x = BigDecimal(x)
       y = (x * (BigMath.log(x, prec) - 1)).add(BigMath.log(2 * BigMath::PI(prec).div(x, prec), prec) / 2, prec)
@@ -393,7 +422,7 @@ module BigMath
       x2 = x.mult(x, prec)
       (1..).each do |k|
         xn = xn.mult(x2, prec) if k != 1
-        d = bernoulli(2 * k, bns, prec).div(xn, prec).div(2*k*(2*k-1), prec)
+        d = bernoulli(2 * k, bns, prec).div(xn, prec).div(2 * k * (2 * k - 1), prec)
         y = y.add(d, prec)
         break if d.exponent < y.exponent - prec
       end
